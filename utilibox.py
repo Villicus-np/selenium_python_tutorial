1000049a7a0169a2e8dbba8ce18d64c1693fe85d
Kevin Khew <73118508+Villicus-np@users.noreply.github.com>
2023-01-29 20:25:18 +0800

    Update utilibox.py

--- utilibox.py
+++ utilibox.py
@@ -6,9 +6,9 @@ from selenium.webdriver.common.by import By
 
 class Toolbox:
     load_dotenv()
-    secret_username = os.getenv('UNAME')
-    secret_password = os.getenv('PASSWD')
-    invalid_password = os.getenv('INVALID_PASSWD')
+    #secret_username = os.getenv('UNAME')
+    #secret_password = os.getenv('PASSWD')
+    #invalid_password = os.getenv('INVALID_PASSWD')
 
     def url(self):
         self.driver.get(os.getenv('BASE_URL'))
@@ -16,9 +16,9 @@ class Toolbox:
         page_url = self.driver.current_url
         assert os.getenv('BASE_URL') in page_url
 
-    def login(self, uname, pword):
-        self.driver.find_element(By.CSS_SELECTOR, '#signin_button').click()
-        self.driver.find_element(By.CSS_SELECTOR, "#user_login").send_keys(uname)
-        self.driver.find_element(By.CSS_SELECTOR, '#user_password').send_keys(pword)
-        self.driver.find_element(By.CSS_SELECTOR, '#user_remember_me').click()
-        self.driver.find_element(By.CSS_SELECTOR, 'input[value="Sign in"]').click()
+    #def login(self, uname, pword):
+    #    self.driver.find_element(By.CSS_SELECTOR, '#signin_button').click()
+    #    self.driver.find_element(By.CSS_SELECTOR, "#user_login").send_keys(uname)
+    #    self.driver.find_element(By.CSS_SELECTOR, '#user_password').send_keys(pword)
+    #    self.driver.find_element(By.CSS_SELECTOR, '#user_remember_me').click()
+     #   self.driver.find_element(By.CSS_SELECTOR, 'input[value="Sign in"]').click()
